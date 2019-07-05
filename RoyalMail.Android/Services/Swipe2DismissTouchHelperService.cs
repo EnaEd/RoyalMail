7c235bfafb56d7f49d862b37052b10547a8b8605
ena.ed <djaziz12393@gmail.com>
2019-07-05 19:16:57 +0300

    add swipe2Dismiss

---
+++ RoyalMail.Android/Services/Swipe2DismissTouchHelperService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Support.V7.Widget.Helper;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using RoyalMail.Core.Models;
+
+namespace RoyalMail.Android.Services
+{
+    class Swipe2DismissTouchHelperService : ItemTouchHelper.SimpleCallback
+    {
+        private readonly Context _context;
+        private bool _swipeBack;
+        public Swipe2DismissTouchHelperService(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public Swipe2DismissTouchHelperService(Context context) : base(0, ItemTouchHelper.Left)
+        {
+            _context = context;
+            _swipeBack = false;
+        }
+
+        public override bool OnMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target)
+        {
+            return false;
+        }
+
+        public override void OnSwiped(RecyclerView.ViewHolder viewHolder, int direction)
+        {
+            var holder = (MvxRecyclerViewHolder)viewHolder;
+            var item = (Task)holder.DataContext;
+            Toast.MakeText(Application.Context, $"swiped", ToastLength.Long).Show();
+
+        }
+    }
+}
--- RoyalMail.Android/View/TaskListView.cs
+++ RoyalMail.Android/View/TaskListView.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Support.V7.Widget.Helper;
+using MvvmCross.Droid.Support.V7.RecyclerView;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
 using Plugin.CurrentActivity;
+using RoyalMail.Android.Services;
 using RoyalMail.Core.ViewModels;
 
 namespace RoyalMail.Android.View
@@ -12,14 +15,17 @@ namespace RoyalMail.Android.View
     public class TaskListView : MvxActivity<TaskListViewModel>
     {
         private Refractored.Fab.FloatingActionButton _fab;
+        private ItemTouchHelper _itemTouchHelper;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             CrossCurrentActivity.Current.Init(this, bundle);
             SetContentView(Resource.Layout.TaskListView);
-            var listView = FindViewById<MvvmCross.Platforms.Android.Binding.Views.MvxListView>(Android.Resource.Id.list_item);
+            var listView = FindViewById<MvxRecyclerView>(Android.Resource.Id.list_item);
+            _itemTouchHelper = new ItemTouchHelper(new Swipe2DismissTouchHelperService(this));
+            _itemTouchHelper.AttachToRecyclerView(listView);
             _fab = FindViewById<Refractored.Fab.FloatingActionButton>(Resource.Id.fab);
-            _fab.AttachToListView(listView);
+            _fab.AttachToRecyclerView(listView);
 
             _fab.Click += CrateNewTask;
 
